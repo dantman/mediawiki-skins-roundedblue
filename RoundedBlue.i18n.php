862079a8ce2cf28168c192b358d29e4f0caa7884
Daniel Friesen <pub-github@nadir-seen-fire.com>
2012-03-27 13:12:13 -0700

    Basic skin fix up so it just barely runs in recent MediaWiki: - Move roundedblue.php to RoundedBlue.skin.php - Add new roundedblue.php entry file for the require_once from LocalSettings.php - Move roundedblue/ to css/ - Add the i18n file with skin name and description fix error from removal of tooltipAndAccesskey

---
+++ RoundedBlue.i18n.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Internationalization file for skin Rounded Blue.
+ *
+ * @file
+ * @ingroup Skins
+ */
+
+$messages = array();
+
+/** English
+ */
+$messages['en'] = array(
+	'skinname-roundedblue' => "Rounded Blue",
+	'roundedblue-desc' => "An adaptation of Ghyslain Armand's Rounded Blue Edition WordPress theme to MediaWiki.",
+);
